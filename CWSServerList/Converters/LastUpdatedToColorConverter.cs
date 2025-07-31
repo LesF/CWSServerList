68d08d7974db7d3372f599046cafcafbbbb2b3b3
Les Ferguson <Les.Ferguson@waikatodhb.health.nz>
2025-07-31 13:31:37 +1200

    Using AppTheme light and dark colors

--- CWSServerList/Converters/LastUpdatedToColorConverter.cs
+++ CWSServerList/Converters/LastUpdatedToColorConverter.cs
@@ -8,11 +8,21 @@ namespace CWSServerList.Converters
     {
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
+            // AppTheme theme = Application.Current?.RequestedTheme ?? AppTheme.Light;
+            // If system theme changes the app themes switch for most display element, except the Convert method
+            // does not get recalled so colors won't change until the elements using this converter get refreshed.
+
             if (value is DateTime lastUpdated)
             {
-                return (DateTime.Now - lastUpdated).TotalMinutes <= 5 ? Colors.Cyan : Colors.White;
+                return (DateTime.Now - lastUpdated).TotalMinutes <= 5 ? FontAttributes.Bold : FontAttributes.None;
+
+                //if (theme == AppTheme.Dark)
+                //    return (DateTime.Now - lastUpdated).TotalMinutes <= 5 ? Colors.Cyan : Colors.White;
+                //else
+                //    return (DateTime.Now - lastUpdated).TotalMinutes <= 5 ? Colors.BlueViolet : Colors.Black;
             }
-            return Colors.Black;
+            return FontAttributes.None;
+            //return (theme == AppTheme.Dark) ? Colors.White : Colors.Black;
         }
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
